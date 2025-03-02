882a7164063ece9ba111f076c4dc519c0f121c8d
Sana Kashyap <sana.kashyap@sanas-air.lan>
2025-03-01 21:44:39 -0500

    removed progress

--- isefproject2025.py
+++ isefproject2025.py
@@ -48,7 +48,7 @@ def compute_confidence_interval(X_kmer, model, bootstrap_samples=1000):
        # ✅ Streamlit animation
        if _ % 200 == 0:  # Update every 200 iterations
            progress_bar.progress(int((_ / bootstrap_samples) * 100))
-
+   progress_bar.progress(1)
 
    lower_bound, upper_bound = np.percentile(bootstrap_preds, [2.5, 97.5])
    return round(lower_bound, 3), round(upper_bound, 3)
@@ -97,7 +97,6 @@ if st.button("🔍 Predict"):
        # ✅ Confidence Interval Animation
        progress_bar = st.progress(0)
        lower_bound, upper_bound = compute_confidence_interval(X_kmer, model)
-
 
        # ✅ Polyreactivity Categorization
        LOW_THRESHOLD = 0.3
